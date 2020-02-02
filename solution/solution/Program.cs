61645ee0a6cc46a40d80b1e678081c53427e1868
Сергій Руденко <oberc@ukr.net>
2020-02-02 20:30:15 +0000

    Updated Program.cs

--- solution/solution/Program.cs
+++ solution/solution/Program.cs
@@ -6,7 +6,7 @@ namespace solution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
